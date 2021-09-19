f253c104804c92bb1a777944be75afe4d31af073
Saranya Sri <dentist.apassionateprogrammer@gmail.com>
2021-09-19 13:20:58 +0530

    added dispatch action to add and reduce counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,17 @@
 import React from "react";
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 function Counter() {
   const counter = useSelector((state) => {
     return state.counter;
   });
+  const dispatch = useDispatch();
+  const addCounterHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const reduceCounterHandler = () => {
+    dispatch({ type: "decrement" });
+  };
   return (
     <div className={classes.Counter}>
       <h2>Redux Counter</h2>
@@ -12,8 +19,8 @@ function Counter() {
         <p>{counter}</p>
       </div>
       <div className={classes.buttons}>
-        <button>Add</button>
-        <button>Reduce</button>
+        <button onClick={addCounterHandler}>Add</button>
+        <button onClick={reduceCounterHandler}>Reduce</button>
       </div>
     </div>
   );
