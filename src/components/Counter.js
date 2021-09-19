39182444c12813b45b3d838677435670bd8b9c75
Saranya Sri <dentist.apassionateprogrammer@gmail.com>
2021-09-19 11:32:52 +0530

    added redux store and reducer ,then used the useSelector to get the counter value from central store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,15 @@
 import React from "react";
 import classes from "./Counter.module.css";
+import { useSelector } from "react-redux";
 function Counter() {
+  const counter = useSelector((state) => {
+    return state.counter;
+  });
   return (
     <div className={classes.Counter}>
       <h2>Redux Counter</h2>
       <div>
-        <p>counter number</p>
+        <p>{counter}</p>
       </div>
       <div className={classes.buttons}>
         <button>Add</button>
